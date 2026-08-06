d1ed5cc80060adb607ab3db0b472b178cb63ee1d
Tushar Bisht <tusharbisht200626@gmail.com>
2026-08-06 21:41:53 +0530

    Time: 159 ms (21.42%) | Memory: 12.8 MB (59.98%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -3,7 +3,7 @@ class Solution(object):
     def isPalindrome(self, s):
         a=""
         for i in s:
-            if i not in string.punctuation and i!=" " and i!=":":
+            if i not in string.punctuation and i!=" ":
                 a+=i.lower()
         if a==a[::-1]:
             return True
